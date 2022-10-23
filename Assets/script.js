9dedb4b08f24619eef3f0f09e743f979433b9015
Arspeke8 <110306643+Arspeke8@users.noreply.github.com>
2022-10-22 19:54:15 -0700

    added recipe and totals

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+/* Simple is Jimmy John Genoa
+Per Individual Batch
+Wine 9 lbs
+Celery Powder 23.7 lbs.
+Salt 150 lbs
+HPS 1.1 lbs
+Spice 88 lbs
+*/
+function jimmyGenoa() {
+  //# of batches need user input
+  const batches = 27;
+  // set amount recipe per batch
+  const wine = 9;
+  const celeryPowder = 23.7;
+  const salt = 150;
+  const hps = 1.1;
+  const spice = 88;
+  //total by user input
+  const wineTotal = batches * wine;
+  const celeryPowderTotal = batches * celeryPowder;
+  const saltTotal = batches * salt;
+  const hpsTotal = batches * hps;
+  const spiceTotal = batches * spice;
+
+  return;
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kitchen-formulas
+
+# Simple UI nothing fancy
+
+# User from Drop Down Menu list picks what is running for the day
+
+# User Inputs how many batches for each product
+
+# Outline is shown after user clicks enter showing what amount of spices are needed for each day and product.
+
+# Making Supervisor's Job Easier
