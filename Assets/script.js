33d7e64da2ca253b19cd30583c74e641faf765b2
Arspeke8 <110306643+Arspeke8@users.noreply.github.com>
2022-10-23 19:46:32 -0700

    added .value to save fbatch completed two types of Salames

--- Assets/script.js
+++ Assets/script.js
@@ -16,10 +16,23 @@ var getButtonAsal = document.getElementById("a-sal");
 var getButtonBurn = document.getElementById("nb-sal");
 var getButtonRoni = document.getElementById("r-pepp");
 var getButtonAroni = document.getElementById("an-pepp");
+var getButtonLast = document.getElementById("b-oa");
+let numOfbatches = document.getElementById("num-batches");
+let fbatch = numOfbatches.value;
+
+//function finalBatches() {
+//let fbatch = numOfbatches.value;
+//if (fbatch > 0);
+//console.log(fbatch);
+//}
+
+//getButtonLast.addEventListener("click", finalBatches);
 
 function jimmyGenoa() {
+  let fbatch = numOfbatches.value;
+
   //# of batches need user input
-  let batches = 1;
+  //let batches = finalBatches();
   // set amount recipe per batch
   const wine = 9.6;
   const celeryPowder = 23.7;
@@ -27,13 +40,13 @@ function jimmyGenoa() {
   const hps = 1.1;
   const spice = 88;
   //total by user input
-  let wineTotal = batches * wine;
-  let celeryPowderTotal = batches * celeryPowder;
-  let saltTotal = batches * salt;
-  let hpsTotal = batches * hps;
-  let spiceTotal = batches * spice;
+  let wineTotal = fbatch * wine;
+  let celeryPowderTotal = fbatch * celeryPowder;
+  let saltTotal = fbatch * salt;
+  let hpsTotal = fbatch * hps;
+  let spiceTotal = fbatch * spice;
 
-  document.querySelector("#a-a").textContent = batches;
+  document.querySelector("#a-a").textContent = fbatch;
   document.querySelector("#a-b").textContent = wineTotal;
   document.querySelector("#a-c").textContent = saltTotal;
   document.querySelector("#a-d").textContent = spiceTotal;
@@ -41,14 +54,17 @@ function jimmyGenoa() {
   document.querySelector("#a-f").textContent = hpsTotal;
 
   console.log(
-    `For ${batches} batches of JJ Genoa need ${wineTotal} wine pounds, ${celeryPowderTotal} pounds of celery powder, ${saltTotal} pounds of salt, ${hpsTotal} pounds of HPS, and ${spiceTotal} pounds of Spice.`
+    `For ${fbatch} batches of JJ Genoa need ${wineTotal} wine pounds, ${celeryPowderTotal} pounds of celery powder, ${saltTotal} pounds of salt, ${hpsTotal} pounds of HPS, and ${spiceTotal} pounds of Spice.`
   );
 }
+
+//getButtonLast.addEventListener("click", finalBatches);
 getButtonJ.addEventListener("click", jimmyGenoa);
 
 function regularSalame() {
+  let fbatch = numOfbatches.value;
   //# of batches need user input
-  const batches = 1;
+  //const batches = 1;
   // set amount recipe per batch
   const wine = 8;
   const salt = 150;
@@ -56,14 +72,21 @@ function regularSalame() {
   const llCulture = 0.55;
   const spice = 391.2;
   //total by user input
-  const wineTotal = batches * wine;
-  const llTotal = batches * llCulture;
-  const saltTotal = batches * salt;
-  const blcTotal = batches * blc;
-  const spiceTotal = batches * spice;
+  let wineTotal = fbatch * wine;
+  let llTotal = fbatch * llCulture;
+  let saltTotal = fbatch * salt;
+  let blcTotal = fbatch * blc;
+  let spiceTotal = fbatch * spice;
+
+  document.querySelector("#b-a").textContent = fbatch;
+  document.querySelector("#b-b").textContent = wineTotal;
+  document.querySelector("#b-c").textContent = saltTotal;
+  document.querySelector("#b-d").textContent = blcTotal;
+  document.querySelector("#b-e").textContent = llTotal;
+  document.querySelector("#b-f").textContent = spiceTotal;
 
   console.log(
-    `For ${batches} batches of Regular Salame need ${wineTotal} wine pounds, ${llTotal} pounds LL2 Culture, ${blcTotal} pounds of BLC ${saltTotal} pounds of salt, and ${spiceTotal} pounds of Spice.`
+    `For ${fbatch} batches of Regular Salame need ${wineTotal} wine pounds, ${llTotal} pounds LL2 Culture, ${blcTotal} pounds of BLC ${saltTotal} pounds of salt, and ${spiceTotal} pounds of Spice.`
   );
 }
 getButtonReg.addEventListener("click", regularSalame);
